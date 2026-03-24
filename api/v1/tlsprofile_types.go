8be4f18fc2008287f071acb6449b29ec02119f70
Leela Venkaiah G <lgangava@ibm.com>
2026-03-24 10:17:48 +0000

    bump controller-gen for make generate & manifests to work
    remove placeholder text and fix incorrect APIs from PROJECT
    
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- api/v1/tlsprofile_types.go
+++ api/v1/tlsprofile_types.go
@@ -20,22 +20,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // TLSProfileSpec defines the desired state of TLSProfile
 type TLSProfileSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of TLSProfile. Edit tlsprofile_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // TLSProfileStatus defines the observed state of TLSProfile
 type TLSProfileStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
